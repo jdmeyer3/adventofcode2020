a11a541b6f74db1cbad584eefcb56b429a5d1d3f
jdmeyer3 <joshua.meyer@comcast.net>
2020-12-08 12:55:03 -0700

    day6 pt2 finish

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,38 +1,55 @@
 use itertools::Itertools;
 
-// It is # of people, and # of questions
+
 #[derive(Default, Debug)]
-struct Questionnaire(usize, usize, Vec<char>);
+struct Questionnaire(Vec<Vec<char>>);
+
+
+impl Questionnaire {
+    fn answer_everyone(&self) -> usize {
+        if self.0.len() == 1 {
+            return self.0[0].len();
+        }
+        self.0[0].iter().map(|f| self.check_everyone_answer(1, f)).filter(|f| *f).count()
+    }
+
+    fn check_everyone_answer(&self, mut person: usize, answer: &char) -> bool {
+        if person + 1 == self.0.len() {
+            return self.0[person].contains(answer)
+        }
+        if self.0[person].contains(answer) {
+            person += 1;
+            return self.check_everyone_answer(person, answer)
+        }
+        false
+    }
+
+    fn answer_flat(&self) -> usize {
+        let answer: Vec<char> = Vec::new();
+        let foo = self.0.clone().into_iter().flatten().unique().collect::<Vec<char>>();
+        println!("{}", foo.len());
+        foo.len()
+    }
+}
 
 fn read_results(input: String) -> Vec<Questionnaire> {
     let mut q: Vec<Questionnaire> = Vec::new();
     q.push(Questionnaire::default());
-    for l in input.lines() {
-        let q_ptr = q.last_mut().unwrap();
-        if l.is_empty()  {
-            q_ptr.2.iter().unique().collect::<Vec<_>>();
-            q_ptr.1 = q_ptr.2.iter().unique().collect::<Vec<_>>().len();
-            println!("{:?}", q_ptr);
+    for i in input.lines() {
+        if i.is_empty() {
             q.push(Questionnaire::default());
             continue;
         }
-        q_ptr.0 += 1;
-        q_ptr.2.extend_from_slice(&l.chars().collect::<Vec<char>>());
-        println!("{:?}", q_ptr);
-
+        let foo = i.chars().unique().collect::<Vec<char>>();
+        q.last_mut().unwrap().0.push(foo);
     }
-    // done once more since last line doesn't get evaluated
-    let mut tmp_ptr = q.last_mut().unwrap();
-    tmp_ptr.2.iter().unique().collect::<Vec<_>>();
-    tmp_ptr.1 = tmp_ptr.2.iter().unique().collect::<Vec<_>>().len();
-    println!("{:?}", tmp_ptr);
     q
 }
+
 
 fn main() {
     let input = std::fs::read_to_string("input/day6").unwrap();
     let q_results = read_results(input);
-    println!("{:?}", q_results);
-    let sum: usize = q_results.iter().map(|q| q.1).sum();
-    println!("{:?}", sum);
+    println!("total pt1: {:?}", q_results.iter().map(|f| f.answer_flat()).sum::<usize>());
+    println!("total pt2: {:?}", q_results.iter().map(|f| f.answer_everyone()).sum::<usize>());
 }
