3a8a4162464502076317c124855d2798d268aebf
jdmeyer3 <joshua.meyer@comcast.net>
2020-12-13 16:06:09 -0700

    finished day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -16,7 +16,7 @@ struct Seating {
 }
 
 impl fmt::Display for Seating {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut output: String = String::new();
         let mut row = Vec::new();
         for s in self.seats.iter() {
@@ -45,6 +45,76 @@ impl fmt::Display for Seating {
 }
 
 impl Seating {
+    fn visible(&self, cur_pos: usize, direction: &dyn Fn(&Self, usize) -> (Seat, i32)) -> Seat {
+        // println!("checking visible for idx: {}", cur_pos);
+        let (mut next_seat, mut next_pos) = direction(self, cur_pos);
+        if next_seat == Seat::Floor {
+            while next_seat == Seat::Floor {
+                let (_next_seat, _next_pos) = direction(self, next_pos as usize);
+                next_seat = _next_seat;
+                next_pos = _next_pos;
+                // println!("next seat: {:?} next idx: {}", next_seat, next_pos)
+            }
+        }
+        // println!("visible for idx {}:  {:?}", cur_pos, next_seat);
+        return next_seat;
+    }
+    fn rule_occupied(&self, seat: &mut Seat, neighbors: &Vec<Seat>, count: usize) -> bool {
+        if neighbors.iter().filter(|&f| *f == Seat::Occupied).count() >= count &&
+            seat == &Seat::Occupied {
+            return true;
+        }
+        false
+    }
+
+    fn rule_empty(&self, seat: &mut Seat, neighbors: &Vec<Seat>, count: usize) -> bool {
+        if neighbors.iter().filter(|&f| *f == Seat::Occupied).count() == count &&
+            seat == &Seat::Empty {
+            return true;
+        }
+        false
+    }
+
+    fn visible_occupied(&mut self) -> bool {
+        let mut changed = false;
+        let mut new_arrangement: Vec<Seat> = Vec::new();
+        for (i, mut s) in self.seats.clone().iter_mut().enumerate() {
+            // println!("evalutating idx {} seat {:?}", i, s);
+            // short circuit floor
+            if *s == Seat::Floor {
+                new_arrangement.push(Seat::Floor);
+                continue;
+            }
+            let neighbors: Vec<Seat> = vec![
+                self.visible(i, &Seating::left),
+                self.visible(i, &Seating::right),
+                self.visible(i, &Seating::top),
+                self.visible(i, &Seating::top_left),
+                self.visible(i, &Seating::top_right),
+                self.visible(i, &Seating::bottom),
+                self.visible(i, &Seating::bottom_left),
+                self.visible(i, &Seating::bottom_right),
+            ];
+            // println!("seat: {:?} neighbors for idx {}: {:?}", s, i, neighbors);
+            if self.rule_occupied(s, &neighbors, 5) {
+                // println!("idx occupied");
+                new_arrangement.push(Seat::Empty);
+                changed = true;
+                continue;
+            }
+            if self.rule_empty(s, &neighbors, 0) {
+                // println!("idx occupied");
+                new_arrangement.push(Seat::Occupied);
+                changed = true;
+                continue;
+            }
+            new_arrangement.push(s.clone());
+        }
+        self.seats = new_arrangement;
+        changed
+    }
+
+
     fn occupied(&mut self) -> bool {
         let mut changed = false;
         let mut new_arrangement: Vec<Seat> = Vec::new();
@@ -57,26 +127,24 @@ impl Seating {
             }
 
             let neighbors: Vec<Seat> = vec![
-                self.left(i),
-                self.right(i),
-                self.top(i),
-                self.top_left(i),
-                self.top_right(i),
-                self.bottom(i),
-                self.bottom_left(i),
-                self.bottom_right(i)
+                self.left(i).0,
+                self.right(i).0,
+                self.top(i).0,
+                self.top_left(i).0,
+                self.top_right(i).0,
+                self.bottom(i).0,
+                self.bottom_left(i).0,
+                self.bottom_right(i).0,
             ];
             if i == 30 {
-                println!("neighbors for idx {}:  {:?}", i,  neighbors);
+                println!("neighbors for idx {}:  {:?}", i, neighbors);
             }
-            if neighbors.iter().filter(|&f| *f == Seat::Occupied).count() >= 4 &&
-                *s == Seat::Occupied {
+            if self.rule_occupied(s, &neighbors, 4) {
                 new_arrangement.push(Seat::Empty);
                 changed = true;
                 continue;
             }
-            if neighbors.iter().filter(|&f| *f == Seat::Occupied).count() == 0 &&
-                *s == Seat::Empty {
+            if self.rule_empty(s, &neighbors, 0) {
                 new_arrangement.push(Seat::Occupied);
                 changed = true;
                 continue;
@@ -114,60 +182,102 @@ impl Seating {
         }
         self.arr_boundary_check(next_pos)
     }
-    fn left(&self, cur_pos: usize) -> Seat {
-        if self.row_boundary_check(cur_pos, cur_pos as i32 - 1) {
-            return self.is_occupied(cur_pos - 1);
+    fn left(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos as i32 - 1;
+        if self.row_boundary_check(cur_pos, next_pos) {
+            return (self.is_occupied(next_pos as usize), next_pos);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos)
     }
-    fn right(&self, cur_pos: usize) -> Seat {
+    fn right(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos as i32 + 1;
         if self.row_boundary_check(cur_pos, cur_pos as i32 + 1) {
-            return self.is_occupied(cur_pos + 1);
+            return (self.is_occupied(cur_pos + 1), next_pos);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos)
     }
-    fn top(&self, cur_pos: usize) -> Seat {
-        if self.arr_boundary_check(cur_pos as i32 - self.width as i32) {
-            return self.is_occupied(cur_pos - self.width);
+    fn top(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos as i32 - self.width as i32;
+        if self.arr_boundary_check(next_pos) {
+            return (self.is_occupied(next_pos as usize), next_pos);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos)
     }
-    fn top_left(&self, cur_pos: usize) -> Seat {
-        if self.row_boundary_check((cur_pos as i32 - self.width as i32) as usize, (cur_pos as i32 - self.width as i32 - 1)) {
-            return self.is_occupied(cur_pos - self.width - 1);
+    fn top_left(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos as i32 - self.width as i32 - 1;
+        if self.row_boundary_check((cur_pos as i32 - self.width as i32) as usize, next_pos) {
+            return (self.is_occupied(next_pos as usize), next_pos);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos)
     }
-    fn top_right(&self, cur_pos: usize) -> Seat {
-        if self.arr_boundary_check(cur_pos as i32  - self.width as i32) &&
-            self.row_boundary_check((cur_pos - self.width) as usize, (cur_pos as i32 - self.width as i32 + 1)) {
-            return self.is_occupied((cur_pos as i32 - self.width as i32 + 1)as usize);
+    fn top_right(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos as i32 - self.width as i32 + 1;
+        if self.arr_boundary_check(cur_pos as i32 - self.width as i32) &&
+            self.row_boundary_check((cur_pos - self.width) as usize, next_pos) {
+            return (self.is_occupied((next_pos) as usize), next_pos);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos)
     }
-    fn bottom(&self, cur_pos: usize) -> Seat {
-        if self.arr_boundary_check((cur_pos + self.width) as i32) {
-            return self.is_occupied(cur_pos + self.width);
+    fn bottom(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos + self.width;
+        if self.arr_boundary_check(next_pos as i32) {
+            return (self.is_occupied(next_pos), next_pos as i32);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos as i32)
     }
-    fn bottom_left(&self, cur_pos: usize) -> Seat {
-        if self.arr_boundary_check((cur_pos  + self.width) as i32) &&
-            self.row_boundary_check((cur_pos  + self.width) as usize, (cur_pos + self.width - 1) as i32) {
-            return self.is_occupied(cur_pos + self.width - 1);
-        }
-        Seat::OutOfBounds
-    }
-    fn bottom_right(&self, cur_pos: usize) -> Seat {
+    fn bottom_left(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos + self.width - 1;
         if self.arr_boundary_check((cur_pos + self.width) as i32) &&
-            self.row_boundary_check((cur_pos + self.width) as usize, (cur_pos + self.width + 1) as i32) {
-            return self.is_occupied(cur_pos + self.width + 1);
+            self.row_boundary_check((cur_pos + self.width) as usize, next_pos as i32) {
+            return (self.is_occupied(cur_pos + self.width - 1), next_pos as i32);
         }
-        Seat::OutOfBounds
+        (Seat::OutOfBounds, next_pos as i32)
+    }
+    fn bottom_right(&self, cur_pos: usize) -> (Seat, i32) {
+        let next_pos = cur_pos + self.width + 1;
+        if self.arr_boundary_check((cur_pos + self.width) as i32) &&
+            self.row_boundary_check((cur_pos + self.width) as usize, next_pos as i32) {
+            return (self.is_occupied(cur_pos + self.width + 1), next_pos as i32);
+        }
+        (Seat::OutOfBounds, next_pos as i32)
     }
 }
 
 fn main() {
+    // let input = std::fs::read_to_string("input/day11").unwrap();
+    // let mut width = 0;
+    // let mut seats: Vec<Seat> = Vec::new();
+    // for l in input.lines() {
+    //     if width == 0 {
+    //         width = l.len();
+    //     }
+    //     seats.append(&mut l.chars().map(|c| match c {
+    //         'L' => Seat::Empty,
+    //         '.' => Seat::Floor,
+    //         '#' => Seat::Occupied,
+    //         _ => panic!("unknown char")
+    //     }).collect::<Vec<_>>());
+    // }
+    // let mut seating = Seating {
+    //     seats,
+    //     width,
+    // };
+    // println!("starting seating\n{}", seating);
+    // let mut cycles = 0;
+    // loop {
+    //     cycles += 1;
+    //     println!("\n\n\nstarting cycle {}", cycles);
+    //
+    //     let has_changed = seating.occupied();
+    //     println!("{}", seating);
+    //     println!("has changed: {:?}", has_changed);
+    //     if !has_changed {
+    //         break;
+    //     }
+    // }
+    // println!("{:?}", seating.seats.iter().filter(|&f| *f == Seat::Occupied).count())
+
+    // pt2
     let input = std::fs::read_to_string("input/day11").unwrap();
     let mut width = 0;
     let mut seats: Vec<Seat> = Vec::new();
@@ -186,18 +296,19 @@ fn main() {
         seats,
         width,
     };
-    println!("starting seating\n{}", seating);
+    // println!("starting seating\n{}", seating);
     let mut cycles = 0;
     loop {
         cycles += 1;
         println!("\n\n\nstarting cycle {}", cycles);
 
-        let has_changed = seating.occupied();
-        println!("{}", seating);
-        println!("has changed: {:?}", has_changed);
+        let has_changed = seating.visible_occupied();
+        // println!("{}", seating);
+        // println!("has changed: {:?}", has_changed);
         if !has_changed {
-            break
+            break;
         }
     }
     println!("{:?}", seating.seats.iter().filter(|&f| *f == Seat::Occupied).count())
+
 }
